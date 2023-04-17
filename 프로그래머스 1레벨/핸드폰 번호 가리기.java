8ea4dda4372c66e44d074d09388017a5172808fe
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-04-17 18:43:21 +0900

    Create 핸드폰 번호 가리기.java

---
+++ 프로그래머스 1레벨/핸드폰 번호 가리기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String phone_number) {
+        char[] arr = phone_number.toCharArray();
+        for (int i = 0; i < arr.length-4; i++) {
+            arr[i] = '*';
+        }
+        return String.valueOf(arr);
+    }
+}
