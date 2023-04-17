0d870370fa7eb2d97066593bfc637f97089be56a
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-04-17 18:37:17 +0900

    Create 자연수 뒤집어 배열로 만들기.java

---
+++ 프로그래머스 1레벨/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(long n) {
+        int[] answer = new int[(int)Math.log10(n)+1];
+        int i = 0;
+        while (n != 0) {
+            answer[i++] = (int)(n % 10);
+            n /= 10;
+        }
+        return answer;
+    }
+}
