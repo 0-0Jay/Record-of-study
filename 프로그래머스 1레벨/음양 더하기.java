b72913e8fc8a0ab9d0001076b9987675e7fe5a3b
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-04-17 18:43:45 +0900

    Create 음양 더하기.java

---
+++ 프로그래머스 1레벨/음양 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i  = 0; i < signs.length; i++) {
+            if (signs[i] == false) {
+                absolutes[i] = -absolutes[i];
+            }
+            answer += absolutes[i];
+        }
+        return answer;
+    }
+}
