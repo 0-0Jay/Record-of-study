960e975e942b9fac488b393d2d844e2b28614c9a
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-07-13 15:39:44 +0900

    Create eraser.cpp

---
+++ 알고리즘 학원 예제/eraser.cpp
@@ -0,0 +1,29 @@
+// 백준 21756 지우개 : https://www.acmicpc.net/problem/21756
+
+#include <stdio.h>
+
+int arr[101];
+
+int main() {
+	int n, cnt;
+	scanf("%d", &n);
+	cnt = n;
+	while (cnt > 1) {  // 배열에 남은 숫자가 1개가 될 때까지 반복
+		int tmp = 0;
+		for (int i = 1; i <= n; i++) {
+			if (arr[i] == 1) continue;
+			tmp++;
+			if (tmp % 2 == 1) {  // 홀수 번마다 체크
+				arr[i] = 1;
+				cnt--;
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		if (arr[i] == 0) {  // 배열을 탐색해서 0인 숫자 출력 후 break
+			printf("%d", i);
+			break;
+		}
+	}
+	return 0;
+}
