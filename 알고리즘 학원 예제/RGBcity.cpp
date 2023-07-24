4016d78a6c4627fc0bd249c008c46dfc13d795d5
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-07-24 15:30:08 +0900

    Create RGBcity.cpp

---
+++ 알고리즘 학원 예제/RGBcity.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <algorithm>
+#define M 1001
+using namespace std;
+
+int arr[M][3];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d%d%d", &arr[i][0], &arr[i][1], &arr[i][2]);
+		arr[i][0] += min(arr[i - 1][1], arr[i - 1][2]);  // 데이터를 받아서 이전 집에 색칠한 색을 제외한 색들 중 최소값을 누적
+		arr[i][1] += min(arr[i - 1][0], arr[i - 1][2]);
+		arr[i][2] += min(arr[i - 1][0], arr[i - 1][1]);
+	}
+	printf("%d", min(arr[n][0], min(arr[n][1], arr[n][2])));  // 세 집 중 최소값 출력
+	return 0;
+}
