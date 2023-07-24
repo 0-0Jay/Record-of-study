b7a9a71019d5ba7d6dd00648bde0e76c252e6baf
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-07-24 18:23:20 +0900

    Create NM1.cpp

---
+++ 알고리즘 학원 예제/NM1.cpp
@@ -0,0 +1,31 @@
+// 백준 15649번 N과 M(1) : 
+
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+
+int arr[10], chk[10];
+int n, m;
+
+void DFS(int num, int d) {
+	if (d == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	chk[num] = 1;
+	for (int i = 1; i < n + 1; i++) {
+		if (chk[i] == 1) continue;
+		arr[d] = i;
+		DFS(i, d + 1);
+		chk[i] = 0;
+	}
+	chk[num] = 0;
+}
+
+int main() {
+	scanf("%d%d", &n, &m);
+	DFS(0, 0);
+}
