92932408a3642e88885f17d3e85763563d763f52
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-07-26 17:30:33 +0900

    Update alphabet.cpp

--- 알고리즘 학원 예제/alphabet.cpp
+++ 알고리즘 학원 예제/alphabet.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-int maxCount, chk[30], dx[4] = { 0, 1, 0, -1 }, dy[4] = { 1, 0, -1, 0 }, mov[22][22];
+int maxCount, chk[30], dx[4] = { 0, 1, 0, -1 }, dy[4] = { 1, 0, -1, 0 };
 char map[22][22];
 
 void DFS(int d, int x, int y) {
@@ -10,11 +10,9 @@ void DFS(int d, int x, int y) {
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];  // dx배열과 dy배열로 상하좌우 이동을 for문 하나로 묶어서 계산
 		int ny = y + dy[i];
-		if (mov[nx][ny] == 0 && chk[map[nx][ny] - 'A'] == 0 && map[nx][ny] >= 'A' && map[nx][ny] <= 'Z') {  // 범위 벗어나는 경우는 알파벳 범위를 활용해 배제
-			mov[nx][ny] = 1;  // 같은 위치 중복 탐색 방지
+		if (chk[map[nx][ny] - 'A'] == 0 && map[nx][ny] >= 'A' && map[nx][ny] <= 'Z') {  // 범위 벗어나는 경우는 알파벳 범위를 활용해 배제
 			chk[map[nx][ny] - 'A'] = 1;  // 같은 알파벳 중복 탐색 방지
 			DFS(d + 1, nx, ny);
-			mov[nx][ny] = 0;
 			chk[map[nx][ny] - 'A'] = 0;
 		}
 	}
@@ -28,7 +26,6 @@ int main() {
 			scanf(" %c", &map[i][j]);
 		}
 	}
-	mov[1][1] = 1;
 	chk[map[1][1] - 'A'] = 1;  // 시작점 및 시작알파벳 체크
 	DFS(1, 1, 1);
 	printf("%d", maxCount);
